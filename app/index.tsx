1dec62d30d012e599f566181a883eb5ad05dd660
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-12-07 12:49:11 -0500

    Navegacion entre pantallas

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,9 @@
-import {View, Text, SafeAreaView} from 'react-native'
 import React from 'react'
+import {Redirect} from "expo-router";
 
 const App = () => {
     return (
-        <SafeAreaView>
-            <View className="mt-6 mx-2.5">
-                <Text className="text-3xl font-work-black text-primary">Hola mundo!</Text>
-                <Text className="text-2xl font-work-medium text-secondary">Hola mundo!</Text>
-                <Text className="text-xl font-work-light text-tertiary">Hola mundo!</Text>
-            </View>
-        </SafeAreaView>
-
+      <Redirect href={'/home'} />
     )
 }
 export default App
