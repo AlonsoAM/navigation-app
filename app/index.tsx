5951e1671d264d701ec1dd1cdb8f58d184376d5e
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-12-07 09:55:49 -0500

    Colores personalizados

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,9 @@ const App = () => {
     return (
         <SafeAreaView>
             <View className="mt-6 mx-2.5">
-                <Text className="text-3xl font-work-black">Hola mundo!</Text>
+                <Text className="text-3xl font-work-black text-primary">Hola mundo!</Text>
+                <Text className="text-2xl font-work-medium text-secondary">Hola mundo!</Text>
+                <Text className="text-xl font-work-light text-tertiary">Hola mundo!</Text>
             </View>
         </SafeAreaView>
 
